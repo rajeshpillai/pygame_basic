b43f8ddbcf764da3c95edd27121b7ce504e82e4f
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-11-15 01:56:36 +0530

    02 Creating clock object

--- Main.py
+++ Main.py
@@ -1,13 +1,23 @@
 import pygame, sys
 
 pygame.init()
-
 screen = pygame.display.set_mode((640,360),0,32)
 
+clock = pygame.time.Clock()
+FPS = 24
+fivesecondinterval = FPS * 5
+totalframes = 0
+
 while True:
+	#PROCESSES
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
 
-	pygame.display.flip()
+
+#PROCESSES
+#LOGIC
+
+totalframes += 1
+
