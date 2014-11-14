8ff45f898eaffd43e2a36b0437355d2c446e2f99
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-11-15 01:50:25 +0530

    01 Creating simple screen in pygame

--- Main.py
+++ Main.py
@@ -1,5 +1,13 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
-print("It worked")
+screen = pygame.display.set_mode((640,360),0,32)
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.flip()
