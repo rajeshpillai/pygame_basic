8b545aa8e5f237de508c4832ea2e7283890bf84a
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-11-15 13:27:23 +0530

    03 Adding color and shape to the screen

--- Main.py
+++ Main.py
@@ -2,11 +2,19 @@ import pygame, sys
 
 pygame.init()
 screen = pygame.display.set_mode((640,360),0,32)
+screen = pygame.display.set_mode((640,480))
 
-clock = pygame.time.Clock()
-FPS = 24
-fivesecondinterval = FPS * 5
-totalframes = 0
+# colors RGB()
+clr1 = (22, 122, 211)
+clr2 = (0, 44, 166)
+clr3 = (34, 55, 245)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+darkBlue = (0,0,128)
+white = (255,255,255)
+black = (0,0,0)
+pink = (255,200,200)
 
 while True:
 	#PROCESSES
@@ -16,8 +24,8 @@ while True:
 			sys.exit()
 
 
-#PROCESSES
-#LOGIC
-
-totalframes += 1
-
+	#PROCESSES
+	#LOGIC
+	screen.fill(white);
+	pygame.draw.line(screen, clr2, (0 , 0), (640 , 360))
+	pygame.display.flip()
