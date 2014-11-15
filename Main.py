73c0860700c5d515a9a609308f65030a3ec76c47
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-11-15 16:16:34 +0530

    04.1 Adding image

--- Main.py
+++ Main.py
@@ -1,22 +1,11 @@
 import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode((640,360),0,32)
-screen = pygame.display.set_mode((640,480))
 
-# colors RGB()
-clr1 = (22, 122, 211)
-clr2 = (0, 44, 166)
-clr3 = (34, 55, 245)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-darkBlue = (0,0,128)
-white = (255,255,255)
-black = (0,0,0)
-pink = (255,200,200)
+WIDTH, HEIGHT = 640, 360
+screen = pygame.display.set_mode((WIDTH,HEIGHT),0,32)
+img_bug = pygame.image.load("bug.png")
 
-i = 0
 while True:
 	#PROCESSES
 	for event in pygame.event.get():
@@ -25,16 +14,5 @@ while True:
 			sys.exit()
 
 
-	#PROCESSES
-	#LOGIC
-	i += 5
-	if i > 255:
-		i %= 255
-
-
-	screen.fill((i,i,i))
-
-	pygame.draw.line(screen, clr2, (0 , 0), (640 , 360))
-	pygame.draw.rect(screen, clr3, (40,40,300,45))
-	pygame.draw.circle(screen, clr1, (350,200), 80, 40)
+	screen.blit(img_bug, (200,200))
 	pygame.display.flip()
