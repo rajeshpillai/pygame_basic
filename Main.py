88dc13347e799858dffe44294efdd5c5088491ce
Rajesh Pillai <pillai.rajesh@gmail.com>
2014-11-15 20:56:57 +0530

    13 Spawning flies/enemies

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ SCREENWIDTH, SCREENHEIGHT = 640, 360
 screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT),0,32)
 clock = pygame.time.Clock()
 FPS = 24
+total_frames = 0
 
 background = pygame.image.load("images/forest.jpg")
 bug = Bug(0,SCREENHEIGHT - 40,40,40,"images/bug.png")
@@ -17,10 +18,12 @@ fly1 = Fly(40, 200, 40,40,"images/fly.png")
 fly2 = Fly(40, 300, 40,40,"images/fly.png")
 fly3 = Fly(40, 400, 40,40,"images/fly.png")
 
+
 while True:
-	process(bug)
+	process(bug, FPS, total_frames)
 	bug.motion(SCREENWIDTH, SCREENHEIGHT)
 	Fly.movement(SCREENWIDTH)
+	total_frames += 1
 	
 	screen.blit(background, (0,0))
 	BaseClass.allsprites.draw(screen)
--- process.py
+++ process.py
@@ -1,6 +1,6 @@
-import pygame, sys
+import pygame, sys, classes, random
 
-def process(bug):
+def process(bug, FPS, total_frames):
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
@@ -24,3 +24,17 @@ def process(bug):
 	if keys[pygame.K_w]:
 		bug.jumping = True
 		
+	spawn(FPS, total_frames)
+
+	
+def spawn(FPS, total_frames):
+	four_seconds = FPS * 4
+	if total_frames % four_seconds == 0:
+		r = random.randint(1, 2)
+		# 50-50 chances of fly starting from either left or right
+		x = 1
+		if r == 2:
+			x = 640 - 40
+
+
+		fly = classes.Fly(x, 130, 40, 35, "images/fly.png")
